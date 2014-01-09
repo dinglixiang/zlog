b17ee702fe4a805baad0788f1bfa09fcb1546db8
dinglixiang <903975580@qq.com>
2014-01-09 21:49:03 +0800

    config topics routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Zlog::Application.routes.draw do
   root "home#index"
+  resources :topics
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
