777ca70aeaf62b7f3e7ad40012e599873ae02846
dinglixiang <903975580@qq.com>
2014-01-14 22:28:11 +0800

    modify deploy and unicorn_init.sh

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,24 @@
 require "bundler/capistrano"
-set :application, "zlog"
-set :repository,  "git@github.com:dinglixiang/zlog.git"
- 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+require "rvm/capistrano"
 
+server "162.243.230.19", :web, :app, :db, :primary => true
+
+set :application, "zlog"
+set :user, "ding"
+set :port, 22
+set :use_sudo,false 
 set :deploy_to, "/var/www/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, true
+
+set :repository,  "git@github.com:dinglixiang/zlog.git"
+set :scm, :git
 set :branch, "master"
+
 default_run_options[:pty] = true
- 
-server "billie.happyec.org", :web, :app, :db, :primary => true
- 
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
 namespace :deploy do
  
   %w[start stop restart].each do |command|
@@ -24,8 +30,8 @@ namespace :deploy do
  
   desc "things I need to do after deploy:setup"
   task :setup_config, :roles => :app do
-    run "#{try_sudo} ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    run "#{try_sudo} ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    run "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.yml.example"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}. create db"
@@ -37,6 +43,16 @@ namespace :deploy do
   end
   after "deploy:finalize_update", "deploy:symlink_config"
  
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+
 end
 
 # if you want to clean up old releases on each deploy uncomment this:
